43138ad2b4e3d271c7884a83de69414f1bce0c97
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-08-19 19:20:43 +0500

    finish initMap & printMap

--- src/ru/geekbrains/lesson_3/Main.java
+++ src/ru/geekbrains/lesson_3/Main.java
@@ -9,18 +9,64 @@ public class Main {
      */
 
     private static char[][] map; //поле
-    private static int Siza = 3;
+    private static int Size = 3; //размерность
 
-    private static final char DotEmpty = '*';
-    public static final char DotX = 'X';
-    public static final char DoyO = 'O';
+    private static final char DotEmpty = '*'; //символ пустоты
+    public static final char DotX = 'X'; //крестики
+    public static final char DotO = 'O'; //нолики
 
     public static Scanner scaner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
 
+    initMap();
+    printMap();
+
+/*    while (true){
+
+        humanTurn();
+        if (isEndGame(DotX)) {
+            break;
+        }
+
+
+        computerTurn();
+        if (isEndGame(DotO)) {
+            break;
+        }
 
 
     }
+
+        System.out.println("Игра окончена!");
+*/
+    }
+
+    //Метод подготовки поля
+    private static void initMap(){
+        map = new char[Size][Size];
+        for (int i = 0; i < Size; i++) {
+            for (int j = 0; j < Size; j++){
+                map[i][j] = DotEmpty;
+            }
+        }
+    }
+
+    //метод отрисовки поля
+    private static void printMap(){
+        for (int i = 0; i <= Size; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < Size; i++){
+            System.out.print((i + 1) + " " );
+            for (int j = 0; j < Size; j++){
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
 }
