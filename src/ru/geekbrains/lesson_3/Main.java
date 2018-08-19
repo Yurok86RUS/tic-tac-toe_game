64c03137545e92f0d9aab289d95c42705573a459
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-08-19 18:56:19 +0500

    wrote the game sttings

---
+++ src/ru/geekbrains/lesson_3/Main.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.lesson_3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /*
+    настройки
+     */
+
+    private static char[][] map; //поле
+    private static int Siza = 3;
+
+    private static final char DotEmpty = '*';
+    public static final char DotX = 'X';
+    public static final char DoyO = 'O';
+
+    public static Scanner scaner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+    }
+}
